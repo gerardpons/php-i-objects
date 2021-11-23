795c5856362138a373dfd36a5c0c6d0507a931e0
Gerard Pons <gpguisado@gmail.com>
2021-11-23 17:28:00 +0100

    Fixed triangle area formula

--- Shape.php
+++ Shape.php
@@ -23,7 +23,7 @@
     class Triangle extends Shape {
 
         public function area() {
-            return $this->width * $this->height;
+            return ($this->width * $this->height) / 2;
         }
 
     }
